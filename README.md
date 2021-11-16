58296ec0f416fe20191c522eceafd492fa3c6680
czujsnn <czajsonino@gmail.com>
2021-11-16 17:25:53 +0100

    base copy from haproxy

---
+++ README.md
@@ -0,0 +1,4 @@
+# haproxy_flask
+haproxy on flask webserver 
+
+If you want to use loadbalancer on haproxy, swap .cfg (/haproxy/loadbalancer_cfg/haproxy.cfg with /haproxy/haproxy.cfg)
---
+++ backend1/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+
+@app.route("/healthz")
+def healthz():
+    return f"you have hit '/healthz' from backend1 \n hostname is: {socket.gethostname()}"
+
+@app.route("/")
+def default():
+    return f"you have hit '/' from backend1 \n hostname is: {socket.gethostname()}"
+
+@app.route("/metrics")
+def metrics():
+    return f"Metric page for prometheus"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5000)
